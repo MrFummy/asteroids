8f3cc5db448cbf5cea257d397b17cc756e27b252
MrFummy <marcos.garciafdz@gmail.com>
2025-03-05 18:05:56 +0100

    Añadida gestion de objetos mediante grupos

--- main.py
+++ main.py
@@ -9,17 +9,28 @@ def main():
     #print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     reloj = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+    
     jugador = Player(x, y)
+    
     dt = 0
+
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                     return
+        updatable.update(dt)
         screen.fill("black")
-        jugador.draw(screen)
-        jugador.update(dt)
+        #jugador.draw(screen)
+        #jugador.update(dt)
+        for obj in drawable:
+              obj.draw(screen)
+              
         pygame.display.flip()
         dt = reloj.tick(60) / 1000
         
