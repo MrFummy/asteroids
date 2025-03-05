aaca4743c4272e3b34692657a7f5c68a0852b329
MrFummy <marcos.garciafdz@gmail.com>
2025-03-05 17:21:57 +0100

    Creado jugador con movimiento

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -7,12 +8,21 @@ def main():
     #print(f"Screen width: {constants.SCREEN_WIDTH}")
     #print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    reloj = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    jugador = Player(x, y)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                     return
         screen.fill("black")
+        jugador.draw(screen)
+        jugador.update(dt)
         pygame.display.flip()
+        dt = reloj.tick(60) / 1000
+        
 
 if __name__  == "__main__":
         main()  
